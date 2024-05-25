a5903e2e7404cd4b83ef754480a985754629826d
Julien CLAUDEL <julienclaudel51@outlook.com>
2024-05-25 14:39:58 +0200

    Add offline indicator

--- src/Pwa/projects/player-pwa/src/app/modules/player/components/music-list/music-list.component.ts
+++ src/Pwa/projects/player-pwa/src/app/modules/player/components/music-list/music-list.component.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { map, withLatestFrom } from 'rxjs';
 import { PLAYLIST_ID_ROUTER_PARAM } from '../../../../app.routes';
+import { selectIsOfflineOrOfflineForced } from '../../../shared/store/offline-management/offline-management.selectors';
 import { selectAllPlaylists } from '../../core/store/playlists/playlists.selectors';
 import { MusicComponent } from '../music/music.component';
 
@@ -16,6 +17,8 @@ import { MusicComponent } from '../music/music.component';
 export class MusicListComponent {
   private readonly activatedRoute = inject(ActivatedRoute);
   private readonly store = inject(Store);
+
+  public readonly isOfflineOfOfflineForced$ = this.store.select(selectIsOfflineOrOfflineForced);
   public readonly musics$ = this.store.select(selectAllPlaylists).pipe(
     withLatestFrom(this.activatedRoute.paramMap),
     map(([playlists, paramMap]) => {
