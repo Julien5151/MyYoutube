8f731d374ecb8a1529f08b3447eeb7551ec5fc9e
Julien CLAUDEL <julienclaudel51@outlook.com>
2024-05-26 16:48:56 +0200

    Update unit of work attribute

--- src/Application/Attributes/UnitOfWork.cs
+++ src/Application/Attributes/UnitOfWork.cs
@@ -1,4 +1,6 @@
+using System.Data.Common;
 using Infrastructure.Database;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
 namespace Application.Attributes;
@@ -7,10 +9,16 @@ public class UnitOfWork : ActionFilterAttribute
 {
     public override async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
     {
-        // Proceed to the next action filter or action method
         var executedContext = await next();
         var myYoutubeContext = executedContext.HttpContext.RequestServices.GetService<MyYoutubeContext>();
         if (myYoutubeContext is not null && myYoutubeContext.ChangeTracker.HasChanges())
-            await myYoutubeContext.SaveChangesAsync();
+            try
+            {
+                await myYoutubeContext.SaveChangesAsync();
+            }
+            catch (DbException)
+            {
+                context.Result = new BadRequestObjectResult("Something went wrong");
+            }
     }
 }
